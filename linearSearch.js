ee4bce944ebea55931c35e146a75946060486f17
wswalker87 <scott@wscottwalker.com>
2025-11-10 21:25:52 -0600

    Scott Walker Nicholas Van Doren Brian Perry

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,29 @@
 function linearSearch(searchTerm, arr) {
+  for (let item of arr) {
+    if (item === searchTerm) {
+      return arr.indexOf(item)
+    } 
+  }
+  
   return undefined;
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let globalArray = []
+  let index = 0
+  for (let item of arr) {
+    if (item === searchTerm) {
+      console.log(searchTerm)
+      globalArray.push(index)
+      index += 1
+    } else {
+      index += 1
+    }
+  }
+  
+  return globalArray;
 }
+
+console.log(globalLinearSearch("a", "bananas".split(""))) // ["b", "a", "n", "a", "n", "a", "s"]
 
 module.exports = { linearSearch, globalLinearSearch };
